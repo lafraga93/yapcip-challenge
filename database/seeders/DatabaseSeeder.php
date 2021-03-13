9ce7f4244858330f3abe652bce0b0353a512c02b
Gustavo Fraga <gustavolafraga@gmail.com>
2021-03-12 23:42:27 -0300

    remove unused framework base files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,13 +6,13 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
+    public function run(): void
     {
-        // $this->call('UsersTableSeeder');
+        $this->call([
+            UserTypesSeeder::class,
+            UserSeeder::class,
+            TransactionTypeSeeder::class,
+            TransactionStatusSeeder::class,
+        ]);
     }
 }
