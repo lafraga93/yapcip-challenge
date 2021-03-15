52f15fee1f754a90271e37831f984769363191ff
Gustavo Fraga <gustavolafraga@gmail.com>
2021-03-15 05:07:25 -0300

    initial user module

---
+++ app/Modules/Users/Domain/Repositories/Persistence/UserRepositoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Users\Domain\Repositories\Persistence;
+
+interface UserRepositoryInterface
+{
+    public function getUserById(int $userId): object;
+    public function updateUserBalance(float $value): bool;
+}
---
+++ app/Modules/Users/Infrastructure/Repositories/Persistence/UserRepository.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Users\Infrastructure\Repositories\Persistence;
+
+use App\Modules\Users\Domain\Repositories\Persistence\UserRepositoryInterface;
+
+final class UserRepository implements UserRepositoryInterface
+{
+    public function getUserById(int $userId): object
+    {
+        return (object) [];
+    }
+
+    public function updateUserBalance(float $value): bool
+    {
+        return true;
+    }
+}
