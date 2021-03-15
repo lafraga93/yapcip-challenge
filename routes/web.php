c824244372bb3674e80e19e14247a2a3ea7d89bd
Gustavo Fraga <gustavolafraga@gmail.com>
2021-03-15 05:01:50 -0300

    remove unused comments

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,7 @@
 <?php
 
-/** @var \Laravel\Lumen\Routing\Router $router */
+declare(strict_types=1);
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+$router->group(['prefix' => 'v1'], function () use ($router) {
+    $router->post('transactions', 'TransactionController@makeTransaction');
 });
