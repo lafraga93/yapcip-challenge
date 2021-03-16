bb0079a3bd3f477f19dbf3422f41b7048e524061
Gustavo Fraga <gustavolafraga@gmail.com>
2021-03-16 05:45:32 -0300

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ declare(strict_types=1);
 
 $router->group(['prefix' => 'v1'], function () use ($router) {
     $router->post('transactions', 'TransactionController@makeTransaction');
+    $router->post('transactions/reverse', 'TransactionController@reverse');
 });
